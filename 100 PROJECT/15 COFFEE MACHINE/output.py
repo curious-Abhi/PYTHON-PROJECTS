732f39a449958998cb35d25553c87ffa918c4484
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-02-21 22:43:32 +0530

    files updated

---
+++ 100 PROJECT/15 COFFEE MACHINE/output.py
@@ -0,0 +1,28 @@
+'''What would you like? (espresso/latte/cappuccino):latte
+Please insert the coin!
+How much Quarters?1
+How much Dimes?1
+How much Nickles?1
+How much Pennies?1
+Sorry that's not enough money. Money refunded.
+What would you like? (espresso/latte/cappuccino):report
+Water:300ml
+Milk: 200ml
+Coffee: 100g
+Money:$0
+What would you like? (espresso/latte/cappuccino):latte
+Please insert the coin!
+How much Quarters?12
+How much Dimes?12
+How much Nickles?12
+How much Pennies?12
+Here is $2.42 in change
+Here is your latte ☕
+Here is your latte ☕
+Here is your latte ☕
+What would you like? (espresso/latte/cappuccino):report
+Water:100ml
+Milk: 50ml
+Coffee: 76g
+Money:$2.5
+What would you like? (espresso/latte/cappuccino):'''
